6848d61c9543f00cd286247208a8f1348b6556a4
barry <barry@water.com>
2020-04-12 13:59:19 +0800

    Initial commit...

---
+++ index/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import *
+
+
+# Create your views here.
+def indexView(request):
+    # search_song = Dynamic.objects.select_related('song').order_by('-dynamic_plays').all()[:8]
+    #
+    # # 热门搜索，热门下载
+    # search_ranking = search_song[:6]
+
+    return render(request, 'index.html', locals())
