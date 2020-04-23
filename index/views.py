0e9ce905ab58a27f989bf9d95ebceb1c0200b0f1
barry <barry@water.com>
2020-04-23 22:54:38 +0800

    1.Load title logo; 2.Complete title_base.html, search.html; 3.Add app 'play'.

--- index/views.py
+++ index/views.py
@@ -4,7 +4,7 @@ from .models import *
 
 # Create your views here.
 def indexView(request):
-    # search_song = Dynamic.objects.select_related('song').order_by('-dynamic_plays').all()[:8]
+    search_song = Dynamic.objects.select_related('song').order_by('-dynamic_plays').all()[:8]
     #
     # # 热门搜索，热门下载
     # search_ranking = search_song[:6]
--- play/views.py
+++ play/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def playView(request, page):
+    return render(request, 'play.html', locals())
